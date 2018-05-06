e894aadce1942340f46a3db6ca4cfa332939e6cd
Nikolaos Tsoukas <nikolaostsoukas@Nikolaoss-iMac.local>
2018-05-06 12:32:24 -0600

    Updating the click events

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,18 +5,19 @@ var nebulaBtn = document.getElementById('add_animal_btn');
 //function to make call to the giphy api : params = search,index
 function callApi(search, index) {
   $.ajax({
-    url: `https://cors-anywhere.herokuapp.com/http://api.giphy.com/v1/gifs/search?q=${search}&limit=6&api_key=Y7eIT0AhbEPOy1v9fu1UgUozsYu2DDBm`,
+    url: `https://cors-anywhere.herokuapp.com/http://api.giphy.com/v1/gifs/search?q=${search}&limit=10&api_key=Y7eIT0AhbEPOy1v9fu1UgUozsYu2DDBm`,
     method: "GET"
   }).then(function(res) {
     console.log(res);
 
     //1: Get array of objects from data 2. Iterates and builds template. 3. Prepends to DOM
     var arrayOfGif = res.data;
+    $("#images").empty();
     arrayOfGif.forEach(function(val, index) {
       var template = `<div class="green">
-                      <img id="gif${index}" class="gif" src="${val.images.fixed_height_still.url}" data-state="still" data-still="${val.images.fixed_height_still.url}" data-animate="${val.images.fixed_height.url}" />
-                      <a target="_blank" class="download" href="#" onclick="this.href = $('#gif${index}').attr('src');"download><i class="material-icons">cloud_download</i</a>
-                    </div>`;
+                        <img id="gif${index}" class="gif" src="${val.images.fixed_height_still.url}" data-state="still" data-still="${val.images.fixed_height_still.url}" data-animate="${val.images.fixed_height.url}" />
+                        <a target="_blank" class="download" href="#" onclick="this.href = $('#gif${index}').attr('src');"download><i class="material-icons">cloud_download</i</a>
+                     </div>`;
       $("#images").prepend(template);
     });
     addClickToGifs();
